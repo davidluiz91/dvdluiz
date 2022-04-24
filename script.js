282156a06ba8b7e878963ab5128da5a02e446147
david luiz <dvdluiz1991@gmail.com>
2022-04-24 11:56:55 -0300

    pronto para teste produção

--- script.js
+++ script.js
@@ -5,13 +5,39 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
-
     });
+
+    // typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["Front-end", "UX designer", "YouTuber", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Front-end", "UX designer", "YouTuber", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+
+    // slide-up script
+    // $('.scroll-up-btn').click(function(){
+    //     $('html').animate({scrollTop: 0});
+    // });
+
+
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
